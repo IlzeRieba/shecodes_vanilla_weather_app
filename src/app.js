fc17b8fdb250b836cff0f4243dce45dcaa7565a3
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-04-01 13:34:29 +0100

    removed console log and defined city variable

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#city");
   let descritpionElement = document.querySelector("#current-description");
@@ -38,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "9860ec3dfdc6dad49e4f6a4e8c42cfa9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Ericeira&appid=${apiKey}&units=metric`;
+let city = "Riga";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
