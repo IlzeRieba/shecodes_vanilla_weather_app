b81e75379afc676abad2b104a81383e88a59162b
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-04-01 13:02:21 +0100

    showing current temperature, city, conditions, wind speed and humidity

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#city");
+  let descritpionElement = document.querySelector("#current-description");
+  let windElement = document.querySelector("#wind-speed");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descritpionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+}
+
 let apiKey = "9860ec3dfdc6dad49e4f6a4e8c42cfa9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Ericeira&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Ericeira&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
